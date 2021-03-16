c9386082ec818b928a12a205128bf9e0279ee97c
PauloVLB <vitorlimaborges@gmail.com>
2021-03-16 18:34:55 -0300

    ++classe Usuario ++test Usuario

--- src/models/Usuario.php
+++ src/models/Usuario.php
@@ -1,3 +1,36 @@
 <?php
 
-class Usuario {}
+class Usuario {
+    private $nome;
+    private $senha;
+    private $email;
+    private $saldo;
+    
+    public function getNome(){
+        return $this->nome;
+    }
+    public function setNome($nome) {
+        $this->nome = $nome;
+    }
+    
+    public function getSenha() {
+        return $this->senha;
+    }
+    public function setSenha($senha) {
+        $this->senha = $senha;
+    }
+    
+    public function getEmail() {
+        return $this->email;
+    }
+    public function setEmail($email) {
+        $this->email = $email;
+    }
+    
+    public function getSaldo() {
+        return $this->saldo;
+    }
+    public function setSaldo($saldo) {
+        $this->saldo = $saldo;
+    }
+}
