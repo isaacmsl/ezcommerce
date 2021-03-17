c1904501e4ea9b1e0ef7419a5e6cf4c249bd3915
PauloVLB <vitorlimaborges@gmail.com>
2021-03-17 18:57:25 -0300

    criada a possibilidade de usar um banco de teste

--- src/data/sqlite/SQLiteConnection.php
+++ src/data/sqlite/SQLiteConnection.php
@@ -10,14 +10,18 @@ class SQLiteConnection {
      * @var type 
      */
     private $pdo;
-
+    private $isTest = false;
     /**
      * return in instance of the PDO object that connects to the SQLite database
      * @return \PDO
      */
+    public function useTestDatabase() {
+        $this->isTest = true;
+    }
     public function connect() {
         if ($this->pdo == null) {
-            $this->pdo = new \PDO("sqlite:" . dirname(__FILE__).'/../banco.db');
+            $path = $this->isTest ? "bancoTeste.db" : "banco.db";
+            $this->pdo = new \PDO("sqlite:" . dirname(__FILE__)."/../$path");
         }
         return $this->pdo;
     }
