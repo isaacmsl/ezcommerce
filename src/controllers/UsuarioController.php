dc55eca6d155bdb03f034d8884998acdbd71a255
PauloVLB <vitorlimaborges@gmail.com>
2021-03-21 00:16:35 -0300

    ++usuario controler (versao incial) ++usuario actions

---
+++ src/controllers/UsuarioController.php
@@ -0,0 +1,36 @@
+<?php 
+require_once dirname(__FILE__).'/../../vendor/autoload.php';
+
+
+class UsuarioController{
+    public function criar($params) {
+        $u = new Usuario();
+
+        $u->setNome($params['nomeCompleto']);
+        $u->setSenha($params['senha']);
+        $u->setEmail($params['email']);
+        $u->setSaldo(0.0);
+        
+        return $u->cadastrar();
+    }
+    public function remover($params) {
+        $u = new Usuario();
+        $u->setEmail($params['email']);
+        return $u->remover();
+    }
+    public function alterar($params) {
+        $u = new Usuario();
+
+        $u->setNome($params['nomeCompleto']);
+        $u->setSenha($params['senha']);
+        $u->setEmail($params['email']);
+        $u->setSaldo($params['saldo']);
+
+        return $u->alterar();
+    }
+    
+    public function listarTodos($params) {
+        return $u->listarTodos();
+    }
+}
+
