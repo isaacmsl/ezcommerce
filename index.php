1b77a19d6f0359c02f9d49f24328404f78d13e34
isaacmsl <isaacmarlondasilvalourenco@gmail.com>
2021-03-21 21:34:49 -0300

    ++html semantico na tela inicial

--- index.php
+++ index.php
@@ -7,7 +7,63 @@
     <title>Ezcommerce</title>
 </head>
 <body>
-    <h1>Ezcommerce</h1>
-    <p>Bem vindx ao Ezcommerce, compra e venda fácil!</p> 
+    <nav>
+        <header>
+            <h1>Olá, Lourenço</h1>
+            <a href="/logout.php">
+                <img src="/public/log-out.svg" alt="Ícone de logout">
+                <span>Sair</span>
+            </a>
+        </header>
+        <ul>
+            <li>
+                <img src="/public/home.svg" alt="Ícone da página inicial">
+                <span>Inicial</span>
+            </li>
+            <li>
+                <img src="/public/cart.svg" alt="Ícone de carrinho">
+                <span>Carrinho</span>
+            </li>
+            <li>
+                <img src="/public/user.svg" alt="Ícone do perfil do usuário">
+                <span>Meu perfil</span>
+            </li>
+            <li>
+                <img src="/public/help.svg" alt="Ícone de ajuda">
+                <span>Ajuda</span>
+            </li>
+        </ul>
+    </nav>
+    <main>
+        <header>
+            <div class="form__input__container">
+                <img src="/public/user.svg" alt="Ícone de bússola">
+                <input
+                    type="type"
+                    placeholder="Buscar por nome"
+                ></input>
+            </div>
+
+            <a href="cadastrarProduto.php">
+                <img src="/public/plus.svg" alt="Ícone de adicionar">
+                <span>Adicionar</span>
+            </a>
+        </header>
+        <ul>
+            <li>
+                <header>
+                    <h3>Pizza</h3>
+                    <div>
+                        <img src="/public/heart.svg" alt="Ícone de curtidas">
+                        <span>10</span>
+                    </div>
+                </header>
+                <main>
+                    <h2>R$ 100,00</h2>
+                    <span>por Henrique Pizzas</span>
+                </main>
+            </li>
+        </ul>
+    </main>
 </body>
 </html>
