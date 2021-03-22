7c80fa9e9bc2622ba6393420f3afbcbf04b8af62
PauloVLB <vitorlimaborges@gmail.com>
2021-03-22 01:14:39 -0300

    alterados os testes de produtos

--- tests/ProdutoTest.php
+++ tests/ProdutoTest.php
@@ -12,16 +12,32 @@ class ProdutoTest extends PHPUnit {
 
         $this->assertEquals("Rapadura", $p->getNome());
     }
+
+    //Testando o id do usuario
+    public function testId() {
+        $p = new Produto();
+        $p->setId(3);
+
+        $this->assertEquals(3, $p->getId());
+    }
     
     //Testando a senha do usuario
     public function testValor() {
         $p = new Produto();
-        $p->setSenha(30.4);
+        $p->setValor(30.4);
         
         $this->assertEquals(30.4, $p->getValor());
     }
     
     //Testando o email do Usuario
+    public function testEmailUsuario() {
+        $p = new Produto();
+        $p->setEmailUsuario("a@a");
+        
+        $this->assertEquals("a@a", $p->getEmailUsuario());
+    }
+
+    //Testando o estoque do Usuario
     public function testEstoque() {
         $p = new Produto();
         $p->setEstoque(5);
@@ -29,11 +45,19 @@ class ProdutoTest extends PHPUnit {
         $this->assertEquals(5, $p->getEstoque());
     }
     
-    //Testando o saldo do Usuario
+    //Testando a quantidade de curtidas do Usuario
     public function testQntCurtidas() {
         $p = new Produto();
         $p->setQntCurtidas(100);
         
         $this->assertEquals(100, $p->getQntCurtidas());
     }
+    /*
+    public function testUrlImg() {
+        $p = new Produto();
+        $p->setUrlImg("Isaac cagando");
+        
+        $this->assertEquals("Isaac cagando", $p->getUrlImg());
+    }
+    */
 }
