193e069d7b45ee29321370e6f3515b32c65e25d8
PauloVLB <vitorlimaborges@gmail.com>
2021-03-22 17:53:07 -0300

    ++sessions no LoginController inicial / logout

--- src/controllers/LoginController.php
+++ src/controllers/LoginController.php
@@ -6,23 +6,34 @@ class LoginController {
     public function index($params) {
         $email = $params['email'];
         $senha = $params['senha'];
+        
         $u = new Usuario();
-        
         $u->setEmail($email);
-        
+         
         $usuarioMatch = $u->listarPorEmail();
+
+        session_start();
 
         if (!empty($usuarioMatch)) {
             if ($usuarioMatch['senha'] == $senha) {
-                // usar session aqui
+                $usuarioLogado = new Usuario();
+
+                $usuarioLogado->setNome($usuarioMatch["nome"]);
+                $usuarioLogado->setEmail($usuarioMatch["email"]);
+                $usuarioLogado->setSenha($usuarioMatch["senha"]);
+                $usuarioLogado->setSaldo($usuarioMatch["saldo"]);
+                
+                //$usuarioSerializado = serialize($usuarioLogado);
+                
+                $_SESSION["usuario"] = "isaac";
                 header("Location: ../../");
             } else {
-                // session error senha incorreta
+                $_SESSION["error"] = "Email e/ou senha incorretos";                 
                 header("Location: ../../login.php");
             }
         } else {
-            // $_SESSION['error'] = isso ai em baixo
-            // session error email e/ou senha incorretos
+ 
+            $_SESSION["error"] = "Email e/ou senha incorretos";           
             header("Location: ../../login.php");
         }
     }
