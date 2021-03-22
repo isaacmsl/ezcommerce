498650b624640ff7db4ca19e944eb4e8d5692d31
PauloVLB <vitorlimaborges@gmail.com>
2021-03-22 02:57:38 -0300

    ++actions ++controller(so criar) alterado .env de ImgBB

---
+++ src/actions/produto.php
@@ -0,0 +1,19 @@
+<?php 
+
+require_once dirname(__FILE__).'/../controllers/ProdutoController.php';
+
+$produtoController = new ProdutoController();
+
+$acao = $_REQUEST['acao'];
+
+$_REQUEST['imgProduto'] = $_FILES['imgProduto'];
+
+$metodosSuportados = ['criar', 'remover', 'alterar', 'listarTodos', 'listarPorId'];
+
+if(in_array($acao, $metodosSuportados)) {
+    $produtoController->$acao($_REQUEST);
+} else {
+    echo 'tem nao';
+}
+
+header("Location: ../../index.php");
---
+++ src/controllers/ProdutoController.php
@@ -0,0 +1,59 @@
+<?php 
+require_once dirname(__FILE__).'/../../vendor/autoload.php';
+
+if (!isset($_ENV["IMGBB_API_KEY"])) {
+    $dotenv = Dotenv\Dotenv::createImmutable(dirname(__FILE__).'/../../');
+    $dotenv->load();
+    $dotenv->required("IMGBB_API_KEY")->notEmpty();
+} 
+
+class ProdutoController{
+    public function criar($params) {
+        $p = new Produto();
+        
+        $p->setNome($params['nomeProduto']);
+        $p->setValor($params['preco']);
+        $p->setEstoque($params['estoque']);
+        $p->setEmailUsuario($params['emailUsuario']);
+        $p->setQntCurtidas(0);
+    
+        $imgBB = new ImgBB($_ENV["IMGBB_API_KEY"]);
+        $imgUri = $params['imgProduto']['tmp_name'];
+        $imgName = $params['imgProduto']['name'];
+
+        $resultUrl = $imgBB->upload($imgUri, $imgName);
+
+        print_r("\n$resultUrl\n");
+
+        $p->setUrlImg($resultUrl);
+        
+        return $p->cadastrar();
+    }
+    public function remover($params) {
+        $p = new Produto();
+        $p->setEmail($params['email']);
+        return $p->remover();
+    }
+    public function alterar($params) {
+        $p = new Produto();
+
+        $p->setNome($params['nomeProduto']);
+        $p->setValor($params['preco']);
+        $p->setEstoque($params['estoque']);
+        $p->setUrlImg($params['imgProduto']);
+
+        return $p->alterar();
+    }
+    
+    public function listarTodos($params) {
+        $p = new Produto();
+        return $p->listarTodos();
+    }
+
+    public function listarPorId($params) {
+        $p = new Produto();
+        $p->setiD($params['id']);
+        return $p->listarPorEmail();
+    }
+}
+
