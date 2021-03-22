f4295bf807d6c29829bca5dc959ad6dcb9f967c9
isaacmsl <isaacmarlondasilvalourenco@gmail.com>
2021-03-21 23:14:19 -0300

    ++depends css na pagina inicial

--- index.php
+++ index.php
@@ -5,62 +5,193 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Ezcommerce</title>
+
+    <!-- STYLES -->
+    <link rel="stylesheet" href="/styles/global.css">
+    <link rel="stylesheet" href="/styles/cards/produto/produto.css">
+    <link rel="stylesheet" href="/styles/pages/inicial.css">
+    <link rel="stylesheet" href="/styles/btns/cta.css">
+    <link rel="stylesheet" href="/styles/nav/nav.css">
+
+    <!-- Aqui utilizamos apenas o elemento input__container, mas acabamos importando todas as outras (Refatorar!) -->
+    <link rel="stylesheet" href="/styles/form/form.css">
 </head>
 <body>
-    <nav>
-        <header>
+    <nav class="nav">
+        <header class="nav__header">
             <h1>Olá, Lourenço</h1>
-            <a href="/logout.php">
-                <img src="/public/log-out.svg" alt="Ícone de logout">
+            <a 
+                class="nav__header__sair"
+                href="/logout.php"
+            >
+                <img src="/public/log-out.svg" width="24px" alt="Ícone de logout">
                 <span>Sair</span>
             </a>
         </header>
-        <ul>
+        <ul class="nav__header__btns">
             <li>
-                <img src="/public/home.svg" alt="Ícone da página inicial">
-                <span>Inicial</span>
+                <a href="/">
+                    <img src="/public/home.svg" alt="Ícone da página inicial">
+                    <span>Inicial</span>
+                </a>
             </li>
             <li>
-                <img src="/public/cart.svg" alt="Ícone de carrinho">
-                <span>Carrinho</span>
+                <a href="/carrinho.php">
+                    <img src="/public/cart.svg" alt="Ícone de carrinho">
+                    <span>Carrinho</span>
+                </a>
             </li>
             <li>
-                <img src="/public/user.svg" alt="Ícone do perfil do usuário">
-                <span>Meu perfil</span>
+                <a href="/perfil.php">
+                    <img src="/public/user.svg" alt="Ícone do perfil do usuário">
+                    <span>Meu perfil</span>
+                </a>
             </li>
             <li>
-                <img src="/public/help.svg" alt="Ícone de ajuda">
-                <span>Ajuda</span>
+                <a href="/ajuda.php">
+                    <img src="/public/help.svg" alt="Ícone de ajuda">
+                    <span>Ajuda</span>
+                </a>
             </li>
         </ul>
+        <footer class="nav__bottom__logo">  
+            <img src="/public/logo-ez-dark.svg" alt="Logo da Ezcommerce">
+        </footer>
     </nav>
     <main>
         <header>
             <div class="form__input__container">
-                <img src="/public/user.svg" alt="Ícone de bússola">
+                <img src="/public/search.svg" alt="Ícone de buscar produto por nome">
                 <input
                     type="type"
                     placeholder="Buscar por nome"
                 ></input>
             </div>
 
-            <a href="cadastrarProduto.php">
+            <a class="btnCTA" href="cadastrarProduto.php">
                 <img src="/public/plus.svg" alt="Ícone de adicionar">
                 <span>Adicionar</span>
             </a>
         </header>
         <ul>
-            <li>
-                <header>
-                    <h3>Pizza</h3>
+            <li class="cardProduto">
+                <img 
+                    class="cardProduto__imagem"
+                    src="/public/pizza-napolitana.jpg" 
+                    alt="Imagem do produto"
+                >
+                <main class="cardProduto__conteudo">
+                    <header class="cardProduto__conteudo__header">
+                        <h3>Pizza</h3>
+                        <div class="cardProduto__conteudo__curtidas">
+                            <span>10</span>
+                            <img src="/public/heart.svg" alt="Ícone de curtidas">
+                        </div>
+                    </header>
+                    <div class="cardProduto__conteudo__info">
+                        <h2>R$ 100,00</h2>
+                        <span>por Henrique Pizzas</span>
                     <div>
-                        <img src="/public/heart.svg" alt="Ícone de curtidas">
-                        <span>10</span>
-                    </div>
-                </header>
-                <main>
-                    <h2>R$ 100,00</h2>
-                    <span>por Henrique Pizzas</span>
+                </main>
+            </li>
+            <li class="cardProduto">
+                <img 
+                    class="cardProduto__imagem"
+                    src="/public/pizza-napolitana.jpg" 
+                    alt="Imagem do produto"
+                >
+                <main class="cardProduto__conteudo">
+                    <header class="cardProduto__conteudo__header">
+                        <h3>Pizza</h3>
+                        <div class="cardProduto__conteudo__curtidas">
+                            <span>10</span>
+                            <img src="/public/heart.svg" alt="Ícone de curtidas">
+                        </div>
+                    </header>
+                    <div class="cardProduto__conteudo__info">
+                        <h2>R$ 100,00</h2>
+                        <span>por Henrique Pizzas</span>
+                    <div>
+                </main>
+            </li>
+            <li class="cardProduto">
+                <img 
+                    class="cardProduto__imagem"
+                    src="/public/pizza-napolitana.jpg" 
+                    alt="Imagem do produto"
+                >
+                <main class="cardProduto__conteudo">
+                    <header class="cardProduto__conteudo__header">
+                        <h3>Pizza</h3>
+                        <div class="cardProduto__conteudo__curtidas">
+                            <span>10</span>
+                            <img src="/public/heart.svg" alt="Ícone de curtidas">
+                        </div>
+                    </header>
+                    <div class="cardProduto__conteudo__info">
+                        <h2>R$ 100,00</h2>
+                        <span>por Henrique Pizzas</span>
+                    <div>
+                </main>
+            </li>
+            <li class="cardProduto">
+                <img 
+                    class="cardProduto__imagem"
+                    src="/public/pizza-napolitana.jpg" 
+                    alt="Imagem do produto"
+                >
+                <main class="cardProduto__conteudo">
+                    <header class="cardProduto__conteudo__header">
+                        <h3>Pizza</h3>
+                        <div class="cardProduto__conteudo__curtidas">
+                            <span>10</span>
+                            <img src="/public/heart.svg" alt="Ícone de curtidas">
+                        </div>
+                    </header>
+                    <div class="cardProduto__conteudo__info">
+                        <h2>R$ 100,00</h2>
+                        <span>por Henrique Pizzas</span>
+                    <div>
+                </main>
+            </li>
+            <li class="cardProduto">
+                <img 
+                    class="cardProduto__imagem"
+                    src="/public/pizza-napolitana.jpg" 
+                    alt="Imagem do produto"
+                >
+                <main class="cardProduto__conteudo">
+                    <header class="cardProduto__conteudo__header">
+                        <h3>Pizza</h3>
+                        <div class="cardProduto__conteudo__curtidas">
+                            <span>10</span>
+                            <img src="/public/heart.svg" alt="Ícone de curtidas">
+                        </div>
+                    </header>
+                    <div class="cardProduto__conteudo__info">
+                        <h2>R$ 100,00</h2>
+                        <span>por Henrique Pizzas</span>
+                    <div>
+                </main>
+            </li>
+            <li class="cardProduto">
+                <img 
+                    class="cardProduto__imagem"
+                    src="/public/pizza-napolitana.jpg" 
+                    alt="Imagem do produto"
+                >
+                <main class="cardProduto__conteudo">
+                    <header class="cardProduto__conteudo__header">
+                        <h3>Pizza</h3>
+                        <div class="cardProduto__conteudo__curtidas">
+                            <span>10</span>
+                            <img src="/public/heart.svg" alt="Ícone de curtidas">
+                        </div>
+                    </header>
+                    <div class="cardProduto__conteudo__info">
+                        <h2>R$ 100,00</h2>
+                        <span>por Henrique Pizzas</span>
+                    <div>
                 </main>
             </li>
         </ul>
