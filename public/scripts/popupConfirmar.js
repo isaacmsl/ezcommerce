a3dbee5a01dd33a703fbda7515881538fceb341b
isaacmsl <isaacmarlondasilvalourenco@gmail.com>
2021-03-24 16:30:56 -0300

    ++script popupConfirmar

---
+++ public/scripts/popupConfirmar.js
@@ -0,0 +1,22 @@
+function popupConfirmar(msg, action) {
+    if (confirm(msg)) {
+        window.location.href = action
+    }
+}
+
+const formPopup = document.querySelector('form');
+formPopup.addEventListener('submit', event => {
+    event.preventDefault()
+
+    const action = form.getAttribute("action")
+    
+    if (action.includes("remover")) {
+        if(confirm("Deseja realmente deletar?")) {
+            formPopup.submit();
+        }
+    } else if (action.includes("alterar")) {
+        if(confirm("Deseja realmente alterar?")) {
+            formPopup.submit();
+        }
+    }
+})
